2eccba63d4fc9a25c0aa29f05e7b12cec941bfe3
Larry Y <41495222+ly0708@users.noreply.github.com>
2024-03-13 00:54:08 -0500

    new-version

---
+++ js/blogs.js
@@ -0,0 +1,18 @@
+blogs = [
+    {
+        "title": "blog1",
+        "content": "dhsfadjfskdf;os"
+    },
+
+    {
+        "title": "blog2",
+        "content": "dhsfdfsafjdiosajfoadjfos"
+    }
+]
+
+
+let blogs_html = ``;
+
+for(const blog of blogs){
+    
+}
---
+++ js/projects.js
@@ -0,0 +1,36 @@
+var apiUrl = 'https://api.github.com/users/ly0708/repos';
+
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        let repos = data;
+        let projects_html = ``;
+
+        const dont_include_names = new Set(['ly0708', 'todo-list-management', 'ly0708.github.io', 'my-site']);
+
+        for (let repo of repos) {
+            console.log(repo['html_url'])
+            if (repo['fork'] == false && !dont_include_names.has(repo['name'].toLowerCase())) {
+                let temp_html = 
+                `
+                <div class="div-link" onclick="location.href='${repo['html_url']}'">
+                    <p class="sub-title" id="project-link">${repo['name'].toLowerCase()}</p>
+                    <p class="content">${repo['description'].toLowerCase()}</p>
+                </div>
+                `;
+
+                projects_html += temp_html;
+            }
+        }
+
+        let projects = document.querySelector('#projects');
+        projects.innerHTML = projects_html;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
