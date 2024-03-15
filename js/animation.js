b4c37594f6ab123b0fad8ffe0f2b671121ff1f9c
Larry Y <larryyu0116@gmail.com>
2024-03-15 14:27:09 -0500

    animation once

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,15 @@
 
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
-
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    })
-});
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // Unobserve the element after animation
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  }, { once: true }); // Observe the element only once
 
 const hidden_elements = document.querySelectorAll('.hidden');
 hidden_elements.forEach((element) => observer.observe(element));
